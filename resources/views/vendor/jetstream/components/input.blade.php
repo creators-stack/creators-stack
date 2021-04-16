27ce44aafb6df33191afe0e1cdfc342127efd945
BitterTongue <tobhips@protonmail.com>
2021-04-16 22:50:11 +0200

    Add propper creator file upload

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -3,6 +3,6 @@
 
 <input @if ($disabled) 'disabled' @endif {!! $attributes->class([
     'border-gray-300' => !$error,
-    'border-red-300' => $error,
+    'border-red-500' => $error,
     'focus:border-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50 rounded-md shadow-sm',
 ])->merge(['type' => 'text']) !!}>
